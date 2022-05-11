c82efbf2a9a48817cd7913bbe610a5116e89b3c3
Sadat Arefin Rafat <sadatarefin@iut-dhaka.edu>
2022-05-11 19:02:13 +0600

    Header and Form Initial Copy

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='flex justify-start bg-green-700'>
+            <img src="" alt="logo" />
+        </div>
+    );
+};
+
+export default Header;
